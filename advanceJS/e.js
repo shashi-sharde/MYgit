223d4098f2593da81926ed825418173d35ce23fe
shashi-sharde <shashisharde@gmail.com>
2022-11-17 04:56:40 +0530

    Usage of 'THIS' keyword

--- advanceJS/e.js
+++ advanceJS/e.js
@@ -1,77 +1,72 @@
-//Using call function---------------------------
+//house
+//this.table='window table'; //global scope
+//console.log(window.table);
+this.garage={
+    table:'garage table',
+    cleanTable(){
 
-var obj={a:15};
-
-var func=function(b){
-    console.log(this.a +b);
-}
-func.call(obj,5);
-
-// using apply function--------------------------
-
-
-var obj={a:15};
-
-var func=function(b,c){
-    console.log(this.a +b-c);
-}
-var arr=[4,3]
-func.apply(obj,arr);
-
-//using bind function----------------------------
-var obj={a:15};
-
-var func=function(b,c){
-    console.log(this.a +b-c);
-}
-
-var d=func.bind(obj);
-d(5,3);
-
-// student age Question--------------------------
-
-var Student={age:20};
-var Age_of_Student=function(){
-    console.log("age of the student is " +this.age);
-}
-var res=Age_of_Student.bind(Student);
-res();
-
-// Currying
-let multiply =function(x,y){
-    console.log(x*y);
-}
-let multiplyByTwo=multiply.bind(this,2); //here 2  points to x
-multiplyByTwo(5);//here 5 points to y
-// this is called currying as we can create multiple metthod 
-//out of one function and applly for our different purpose.
-let multiplyByThree=multiply.bind(this,3); //here 3  points to x
-multiplyByThree(5);
-
-// say supppose we are passing x and y both at the timeof bind function
-//it will take that as complete parameter and will ignore
-//the parameter if we give while envoking the bind function 
-let multiplyBysix=multiply.bind(this,6,4); //here 6  points to x and 4 points to y 
-multiplyBysix(5);// after taking the input x , y above 
-//it will ignore the argumen we have given while 
-//invoking the fucntion 
-//say suppose we are not passing anythin in the bind function
-//argument and we are passing the values while invoking the function 
-let multiplyByfour=multiply.bind(this); //here 3  points to x
-multiplyByfour(5,4);
-//but this form will not be valid for currying
-//except for the last function we have curried our function in different manner
-
-
-
-//IMPORTANT ##################################################
-// WE CAN ACHIEVE THIS THING BY USING FUNCTION CLOUSURE AS WELL
- let mul=function(x){ //here we are presetting the x and returning the another function which takes the parameter as y 
-    return function(y){
-        console.log('multiple is '+x*y);
+        console.log(`cleaning ${this.table}`);
     }
- }
- let mul2=mul(2); //2 points to x
- mul2(92); //92 points to y 
+};
+this.garage.table; //when you said 'this.' then you are creating a public property
+//this inside an object 
+// let johnRoom={
+//     table:'johns table'
+// };
+// console.log(johnRoom.table);
+// this inside a method
+let johnRoom={
+    table:'johns table',
+    cleanTable(){
 
- //this is also called currying in java script
+        console.log(`cleaning ${this.table}`);
+    }
+};
+console.log(johnRoom.table);
+johnRoom.cleanTable();//caling from method inside the private object
+this.garage.cleanTable() //caling from method inside the public object
+// this inside a function
+// this.table='window table'; //global scope
+// const clean=function(soap){
+//     console.log(`cleaning ${this.table} using ${soap}`);
+// };
+// clean.call(this, 'surf-excel');
+// clean.call(this.garage,'vimbar');
+// clean.call(johnRoom,'harpic');
+
+//this inside an inner function
+this.table='window table'; //global scope
+const clean=function(soap){
+    const inner=(_soap) => {
+        console.log(`cleaning ${this.table} using ${soap}`);
+
+    }
+    //inner.call(this)(soap); or
+    inner(soap);
+
+};
+clean.call(this, 'surf-excel');
+
+//this inside a constructor
+let createRoom=function(name){
+    this.table= `${name}'s room`
+
+}
+const jillsRoom=new createRoom('jill');
+const shashiRoom=new createRoom("shashi");
+clean.call(shashiRoom,'harpic');
+
+//this inside class
+class Room{
+    constructor(name){
+        this.table= `${name}'s table`
+
+
+    }
+    clea(soap){
+
+        console.log(`cleaning ${this.table} using ${soap}`);
+    }
+}
+const illsRoom=new Room('ill');
+illsRoom.clea("lux");
