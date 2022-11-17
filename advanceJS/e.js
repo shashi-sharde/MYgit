874f095a88bd53aac22fb4272b1329d2043fbdb6
shashi-sharde <shashisharde@gmail.com>
2022-11-17 16:57:08 +0530

    Fat arrow function difference with Normal function

--- advanceJS/e.js
+++ advanceJS/e.js
@@ -1,38 +1,20 @@
 class Student{
-   static No_of_student=0;
-    constructor(Name,age,ph_no,marks){
-        this.Name=Name;
+    constructor(name,age,marks){
+        this.name=name;
         this.age=age;
-        this.ph_no=ph_no;
         this.marks=marks;
-        Student.studentsNumber();
     }
-    static studentsNumber(){
-        return (Student.No_of_student++);
-    }
-    
-
-    }Student.prototype.findeligibllity=function findeligibllity(){
-        if(this,this.marks >40){
-            console.log(this.Name + " is eligible")
-        } else{
-            console.log(this.Name + " is not eligible")
+    setPlacementAge(minAgeForPlacement){
+        return (minMarks) => {
+            if (this.marks>minMarks && this.age >minAgeForPlacement){
+                console.log(this.name +" is ready for placement")
+            }else{
+                console.log(this.name +" is not ready for placement")
+            }
         }
     }
+}
 
-const s1=new Student("Shashi",20,1234,89);
-const s2=new Student("Shash",21,12345,39);
-const s3=new Student("Raju",22,123,49);
-const s4=new Student("Raj",26,1236,19);
-const s5=new Student("Rajan",29,1253,69);
-console.log(s1);
-console.log(s2);
-console.log(s3);
-console.log(s4);
-console.log(s5);
-console.log(Student.studentsNumber());
-s1.findeligibllity()
-s2.findeligibllity()
-s3.findeligibllity()
-s4.findeligibllity()
-s5.findeligibllity()
+const shashi=new Student("Shashi",20,85);
+const Shubham=new Student("Shubham",28,62);
+shashi.setPlacementAge(25)(40);
